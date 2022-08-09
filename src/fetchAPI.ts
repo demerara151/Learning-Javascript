6f5d25a138c3de6ce732be130dfdb5930a7d6e35
demerara151 <demeraraonfire@gmail.com>
2022-08-09 16:10:58 +0900

    update fetch function

--- src/fetchAPI.ts
+++ src/fetchAPI.ts
@@ -1,34 +1,38 @@
 // simple fetch function with async-await
 
-interface superHeroes {
-	squadName: string
-	homeTown: string
-	formed: number
-	secretBase: string
-	active: boolean
-	members: Member[]
+interface Heroes {
+  squadName: string;
+  homeTown: string;
+  formed: number;
+  secretBase: string;
+  active: boolean;
+  members: Member[];
 }
 
 interface Member {
-	name: string
-	age: number
-	secretIdentity: string
-	powers: string[]
+  name: string;
+  age: number;
+  secretIdentity: string;
+  powers: string[];
 }
 
-async function getHero(url: string): Promise<void> {
-	const response = await fetch(url)
-	if (response.status !== 200) {
-		console.log("Fetch failed")
-	}
-	const superHeroes: superHeroes = await response.json()
-	console.log(superHeroes.members[0].powers)
-}
+/**
+ * Fetch json data from superheroes.json
+ * @param url target URL
+ */
+const getHero = async (url: string): Promise<void> => {
+  const response = await fetch(url);
+  if (response.status !== 200) {
+    console.log("Fetch failed");
+  }
+  const superHeroes: Heroes = await response.json();
+  console.log(superHeroes.members[0].powers);
+};
 
 getHero(
-	"https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json"
-)
+  "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json",
+);
 
 // all scripts from here will be run whether getHero() is executed or not
 
-console.log("I don't know whether getHero is executed or not.")
+console.log("I don't know whether getHero is executed or not.");
